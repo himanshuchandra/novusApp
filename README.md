9562414e67fb32b0605d92d8a55b86aa238b2372
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-08-19 12:05:34 +0530

    app init

---
+++ README.md
@@ -0,0 +1,2 @@
+"# appskeleton" 
+"# novusApp" 
---
+++ www/js/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appskeleton.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appskeleton
+ */
+angular.module('appskeleton')
+  .controller('MainCtrl', function ($scope) {
+  });
